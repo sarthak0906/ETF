c8ee5def4c9d501b269967d0f181c771f1ba0e30
piyush888 <piyush888@gmail.com>
2020-02-13 14:47:58 +0530

    523List,ETF+Holdings,Both DB working

--- MongoDB/MasterCaller.py
+++ MongoDB/MasterCaller.py
@@ -3,7 +3,7 @@ from MongoDB.Download523TickersList import Download523TickersList
 from MongoDB.DownloadHoldings import DownloadsEtfHoldingsData
 from MongoDB.DataCleanFeed import PullandCleanData
 
-# Download523TickersList().fetchTickerDataDescription()
-# cf.masterclass().save523tickerslisttodb()
-# DownloadsEtfHoldingsData().fetchDataForEtfTicker()
+Download523TickersList().fetchTickerDataDescription()
+cf.masterclass().save523tickerslisttodb()
+DownloadsEtfHoldingsData().fetchDataForEtfTicker()
 PullandCleanData().readfilesandclean()

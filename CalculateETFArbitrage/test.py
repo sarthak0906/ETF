117c38a9caa5d897f59e6f65c56b546387128568
piyush888 <piyush888@gmail.com>
2020-03-06 23:32:28 +0530

    optimizing

--- CalculateETFArbitrage/test.py
+++ CalculateETFArbitrage/test.py
@@ -3,27 +3,25 @@
 #####
 import sys
 
-sys.path.extend(['/home/piyush/Desktop/etf/ETFAnalysis', '/home/piyush/Desktop/etf/ETFAnalysis/ETFsList_Scripts',
-                 '/home/piyush/Desktop/etf/ETFAnalysis/HoldingsDataScripts',
-                 '/home/piyush/Desktop/etf/ETFAnalysis/CalculateETFArbitrage',
-                 '/home/piyush/Desktop/etf/ETFAnalysis/PolygonTickData'])
 sys.path.append("..")
 #####
 # Remove in production - KTZ
 #####
 
-import requests
-from datetime import datetime
-import requests
-import json
-import time
-import pandas as pd
-from time import mktime
 import asyncio
 
 from PolygonTickData.PolygonDataAPIConnection import *
 from CalculateETFArbitrage.helper import Helper
 
+class ETFHoldingsData():
+    def __init__(self, weights, symbols, cashvalue):
+        self.weights = weights
+        self.symbols = symbols
+        self.cashvalue = cashvalue
+
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
 
 class LoadWeightsFromMongoDB(object):
 
@@ -41,12 +39,12 @@ class LoadWeightsFromMongoDB(object):
         # holdings['TickerWeight'] = holdings['TickerWeight'].apply(lambda x: x.replace('%', ''))
         # holdings['TickerWeight'] = holdings['TickerWeight'].astype(float)
         holdings['TickerWeight'] = holdings['TickerWeight'] / 100
-        weights = dict(zip(holdings.TickerSymbol, holdings.TickerWeight))
+        # weights = dict(zip(holdings.TickerSymbol, holdings.TickerWeight))
         cashvalue = holdings[holdings['TickerSymbol'] == 'CASH'].get('TickerWeight').item() * 28583351000
         symbols = list(holdings['TickerSymbol'].values) + ['XLK']
         symbols.remove('CASH')
-
-        etfholdingsdata = {'weights': weights, 'symbols': symbols, 'cashvalue': cashvalue}
+        etfholdingsdata = ETFHoldingsData(dict(zip(holdings.TickerSymbol, holdings.TickerWeight)), symbols + ['XLK'], cashvalue)
+        # etfholdingsdata = {'weights': weights, 'symbols': symbols, 'cashvalue': cashvalue}
         return etfholdingsdata
 
 
@@ -91,8 +89,13 @@ class RunArbitrage(object):
             data['results'] = data['results'] + data2['results']
             # await asyncio.sleep(0.2)
         tickHistData[symbol] = data
-        # print(tickHistData)
-        return tickHistData
+        # print(tickHistData.keys())
+        # print(tickHistData.values())
+        # print(sys.getsizeof(tickHistData))
+        objtickhistdata = objectview(tickHistData)
+        del tickHistData
+        # print(sys.getsizeof(obj))
+        return objtickhistdata
 
     # call this to run the data extractor
     # Calles First
@@ -101,73 +104,52 @@ class RunArbitrage(object):
         # tickers_for = ['XLK', 'VGT', 'FTEC', 'IYW']
         # date_for = datetime.now().date()
         self.etfholdingsdata = LoadWeightsFromMongoDB().getWeightsFromMongoDB()
-        print(self.etfholdingsdata)
+        print(self.etfholdingsdata.symbols)
+        print(self.etfholdingsdata.weights)
+        print(self.etfholdingsdata.cashvalue)
         print(sys.getsizeof(self.etfholdingsdata))
-        tickHistDataQuotes = {}
-        tickHistDataTrade = {}
+
+        tickHistDataQuotes = []
+        tickHistDataTrade = []
         priceforNAVfilling = {}
 
         Polygonobj = PolgonData()
 
         # shorted symbols for testing - remove for production
         # self.etfholdingsdata['symbols']=['TXN','V','XLK']
-
-        #########**************#########
-        # I guess threading will come here, because here we want to thead getting of 1 complete ticker data
-        # So 1 thread for AAPL will complete tickHistDataQuotes,tickHistDataTrade,priceforNAVfilling
-        # So you can have 10 threads for a ticker each. And each thread will complete the get
-        # Challenge will be to store same threads data in same tickHistDataQuotes,tickHistDataTrade, they are sharing resources
-        # Feel free to move below for loop in a seperate method
-        #########**************#########
-
-        # for symbol in self.etfholdingsdata['symbols']:
-        #     tickHistDataQuotes[symbol] = self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricQuotes)
-        #     tickHistDataTrade[symbol] = self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricTrades)
-        #
-        #     # For NA values for a day, fill it with NA values
-        #     dfTemp = Polygonobj.PolygonDailyOpenClose(date=date, symbol=symbol)
-        #     priceforNAVfilling[symbol] = dfTemp['open']
-
-        #########**************#########
-        # I guess threading will come here, because here we want to thead getting of 1 complete ticker data
-        # So 1 thread for AAPL will complete tickHistDataQuotes,tickHistDataTrade,priceforNAVfilling
-        # So you can have 10 threads for a ticker each. And each thread will complete the get
-        # Challenge will be to store same threads data in same tickHistDataQuotes,tickHistDataTrade, they are sharing resources
-        # Feel free to move below for loop in a seperate method
-        #########**************#########
-
-        ###################### ASYNCIO OVER HERE #####################################
+###################### ASYNCIO OVER HERE #####################################
         async def main_():
             async def one_iter(semaphore_, symbol):
                 async with semaphore_:
-                    # print(self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricQuotes))
-                    tickHistDataQuotes[symbol] = await self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricQuotes)
-                    tickHistDataTrade[symbol] = await self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricTrades)
+                    # tickHistDataQuotes[symbol] = await self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricQuotes)
+                    tickHistDataQuotes.append(await self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricQuotes))
+                    print(tickHistDataQuotes)
+                    # print(tickHistDataQuotes[symbol].CTSH)
+                    # tickHistDataTrade[symbol] = await self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricTrades)
+                    tickHistDataTrade.append(await self.getDataFromPolygon(symbol, Polygonobj.PolygonHistoricTrades))
                     dfTemp = Polygonobj.PolygonDailyOpenClose(date=date, symbol=symbol)
                     priceforNAVfilling[symbol] = dfTemp['open']
-
             semaphore = asyncio.BoundedSemaphore(3)
-            co_routines = [one_iter(semaphore, symbol) for symbol in self.etfholdingsdata['symbols']]
+            co_routines = [one_iter(semaphore, symbol) for symbol in self.etfholdingsdata.symbols]
             # co_routines = [one_iter(semaphore, symbol) for symbol in ['AAPL','MSFT','XLK']]
             await asyncio.gather(*co_routines)
-
         loop = asyncio.get_event_loop()
         loop.run_until_complete(main_())
-        ##################### ASYNCIO COMPLETES HERE #################################
+##################### ASYNCIO COMPLETES HERE #################################
         # print(tickHistDataTrade)
 
         helperObj = Helper()
 
         # Convert tickHistDataQuotes to dict
-        tradeData = helperObj.convertDictToFrame(tickHistDataTrade)
-        tradeData = tradeData[['Symbol', 'p', 's', 't', 'x']]
-        tradeData = tradeData[tradeData['s'] != 0]
-
-        # Convert tickHistDataQuotes to dict
-        quotesData = helperObj.convertDictToFrame(tickHistDataQuotes)
-        quotesData = quotesData[['Symbol', 'P', 'S', 'p', 's', 't', 'x', 'X']]
-        quotesData = quotesData[quotesData['S'] != 0]
-        quotesData = quotesData[quotesData['s'] != 0]
+        # tradeData = helperObj.convertDictToFrame(tickHistDataTrade)
+        # tradeData = tradeData[['Symbol', 'p', 's', 't', 'x']]
+        # tradeData = tradeData[tradeData['s'] != 0]
+        #
+        # # Convert tickHistDataQuotes to dict
+        # quotesData = helperObj.convertDictToFrame(tickHistDataQuotes)
+        # quotesData = quotesData[['Symbol', 'P', 'S', 'p', 's', 't', 'x', 'X']]
+        # quotesData = quotesData[quotesData['S'] != 0]
+        # quotesData = quotesData[quotesData['s'] != 0]
 
         # Deleting to save space on disk
         # del tickHistDataTrade
@@ -176,25 +158,27 @@ class RunArbitrage(object):
         #########**************#########
         # Needs Threading in time conversion #
         #########**************#########
-        tradeData['t'] = tradeData['t'].apply(lambda x: helperObj.getHumanTime(x, getMilliSecondsAlso=False))
-        quotesData['t'] = quotesData['t'].apply(lambda x: helperObj.getHumanTime(x, getMilliSecondsAlso=False))
+        # tradeData['t'] = tradeData['t'].apply(lambda x: helperObj.getHumanTimeNew(x))
+        # quotesData['t'] = quotesData['t'].apply(lambda x: helperObj.getHumanTimeNew(x))
+        #
+        # quotesData['Spread'] = quotesData['P'] - quotesData['p']
+        # quotesData['MidPrice'] = (quotesData['P'] + quotesData['p']) / 2
+        #
+        # # Group Trade Data by minutes
+        # tradePrices = tradeData.groupby([tradeData['t'].dt.hour, tradeData['t'].dt.minute, tradeData['Symbol']])[
+        #     'p'].mean()
+        # tradePricesDF = tradePrices.unstack(level=2)
+        # tradePricesDF = tradePricesDF.fillna(method='ffill')
+        # tradePricesDF = tradePricesDF.fillna(priceforNAVfilling)
+        #
+        # # Group Quotes Data by minutes
+        # quotesSpreads = quotesData.groupby([quotesData['t'].dt.hour, quotesData['t'].dt.minute, quotesData['Symbol']])[
+        #     'Spread'].mean()
+        # quotesSpreadDF = quotesSpreads.unstack(level=2)
+        # quotesSpreadDF = quotesSpreadDF.fillna(0)
 
-        quotesData['Spread'] = quotesData['P'] - quotesData['p']
-        quotesData['MidPrice'] = (quotesData['P'] + quotesData['p']) / 2
-
-        # Group Trade Data by minutes
-        tradePrices = tradeData.groupby([tradeData['t'].dt.hour, tradeData['t'].dt.minute, tradeData['Symbol']])[
-            'p'].mean()
-        tradePricesDF = tradePrices.unstack(level=2)
-        tradePricesDF = tradePricesDF.fillna(method='ffill')
-        tradePricesDF = tradePricesDF.fillna(priceforNAVfilling)
-
-        # Group Quotes Data by minutes
-        quotesSpreads = quotesData.groupby([quotesData['t'].dt.hour, quotesData['t'].dt.minute, quotesData['Symbol']])[
-            'Spread'].mean()
-        quotesSpreadDF = quotesSpreads.unstack(level=2)
-        quotesSpreadDF = quotesSpreadDF.fillna(0)
-
+        tradePricesDF = pd.DataFrame()
+        quotesSpreadDF = pd.DataFrame()
         return tradePricesDF, quotesSpreadDF
 
     # Once we have tradePrices and quotesSpread we can call this which will give data for hour for that day

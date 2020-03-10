6e9315410816ecf0819a6021963f2bf5d282d4a0
KshitizSharmaV <kshitizsharmav@gmail.com>
2020-03-10 12:51:03 -0400

    Analysis Fully functioning

--- CalculateETFArbitrage/Control_test2.py
+++ CalculateETFArbitrage/Control_test2.py
@@ -18,6 +18,7 @@ import asyncio
 from CalculateETFArbitrage.helper import Helper
 from CalculateETFArbitrage.GetPolygonData import CallPolygonApi
 from CalculateETFArbitrage.LoadEtfHoldings import LoadHoldingsdata
+from CreateURLSForThreading import CallPolygonApi
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
@@ -26,31 +27,30 @@ logging.basicConfig(filename="Test2Logs.log", format='%(asctime)s %(message)s')
 
 class RunArbitrage(object):
 
-    def __init__(self,etfname):
+    def __init__(self,etfname=None):
         self.etfname=etfname
-        self.date=date
-
+        
         self.tradePricesDFMinutes=None
         self.quotesSpreadsMinutes=None
         self.tradeData=None
         self.quotesData=None        
         
-    def giveArbitrageResults(self,tickHistDataTrade,tickHistDataQuotes):
+    def giveArbitrageResults(self,tradeData=None,quotesData=None,priceforNAVfilling=None):
         helperObj = Helper()
         # Convert tickHistDataQuotes to dict
-        tradeData = helperObj.convertDictToFrame(tickHistDataTrade)
-        tradeData = tradeData[['Symbol', 'p', 's', 't', 'x']]
+        #tradeData = helperObj.convertDictToFrame(tickHistDataTrade)
+        #tradeData = tradeData[['Symbol', 'p', 's', 't', 'x']]
         tradeData = tradeData[tradeData['s'] != 0]
         # Convert tickHistDataQuotes to dict
-        quotesData = helperObj.convertDictToFrame(tickHistDataQuotes)
-        quotesData = quotesData[['Symbol', 'P', 'S', 'p', 's', 't', 'x', 'X']]
+        #quotesData = helperObj.convertDictToFrame(tickHistDataQuotes)
+        #quotesData = quotesData[['Symbol', 'P', 'S', 'p', 's', 't', 'x', 'X']]
         quotesData = quotesData[quotesData['S'] != 0]
         quotesData = quotesData[quotesData['s'] != 0]
         #########**************#########
         # Needs Threading in time conversion #
         #########**************#########
-        tradeData['t'] = tradeData['t'].apply(lambda x: helperObj.getHumanTimeNew(x))
-        quotesData['t'] = quotesData['t'].apply(lambda x: helperObj.getHumanTimeNew(x))
+        tradeData['t'] = tradeData['t'].apply(lambda x: helperObj.getHumanTime(x))
+        quotesData['t'] = quotesData['t'].apply(lambda x: helperObj.getHumanTime(x))
         quotesData['Spread'] = quotesData['P'] - quotesData['p']
         quotesData['MidPrice'] = (quotesData['P'] + quotesData['p']) / 2
         # Group Trade Data by minutes
@@ -63,20 +63,24 @@ class RunArbitrage(object):
         quotesSpreadsMinutes = quotesSpreadsMinutes.unstack(level=2)
         self.quotesSpreadsMinutes = quotesSpreadsMinutes.fillna(0)
 
+        self.quotesSpreadsMinutes.to_csv("quotesByMinutes.csv")
+        self.tradePricesDFMinutes.to_csv("tradesByMinutes.csv")
+
         self.tradeData=tradeData
         self.quotesData=quotesData
 
-    def getMeHourdata(self, getmeHourDataFor=None, tradePricesDF=None, quotesSpreadDF=None):
-        etfspread = quotesSpreadDF[self.etfticker]
-        for name, group in tradePricesDF.groupby(level=0):
+
+    def getMeHourdata(self, getmeHourDataFor=None,etfData=None,tradePricesDFMinutes=None, quotesSpreadsMinutes=None):
+        etfspread = quotesSpreadsMinutes[self.etfname]
+        for name, group in tradePricesDFMinutes.groupby(level=0):
             if name == getmeHourDataFor:
                 break
-        etfprice = group[etfticker]
-        del group[etfticker]
+        etfprice = group[self.etfname]
+        del group[self.etfname]
         group = group.pct_change().dropna() * 100
         etfpricechange = etfprice.pct_change().dropna() * 100
         etfpricechange = etfpricechange.unstack(level=1)
-        netassetvaluereturn = group.assign(**self.etfholdingsdata['weights']).mul(group).sum(axis=1)
+        netassetvaluereturn = group.assign(**etfData.getETFWeights()).mul(group).sum(axis=1)
         netassetvaluereturn = netassetvaluereturn.unstack(level=1)
         ds = pd.concat([etfprice.unstack(level=1), etfpricechange, netassetvaluereturn], axis=0).T
         ds.columns = ['ETF Price', 'ETF Change Price %', 'Net Asset Value Change%']
@@ -87,8 +91,8 @@ class RunArbitrage(object):
 
 if __name__ == "__main__":
     # Create an object of date when we need and time between which we need data
-    previousdate = '2020-02-25'
-    date = '2020-02-26'
+    previousdate = '2020-03-08'
+    date = '2020-03-09'
     starttime = '9:30:00'
     endtime = '17:00:00'
     endtimeLoop = '16:00:00'
@@ -100,16 +104,22 @@ if __name__ == "__main__":
     print(etfData.getETFWeights())
     print(etfData.getCashValue())
 
-    CallPolygonApiObject = CallPolygonApi(date=date,symbols=etfData.getSymbols())
-    tradePricesDF, quotesSpreadDF = CallPolygonApiObject.assemblePolygonData()
+    polygonApi=CallPolygonApi(date=date)
+    tradePricesDF, quotesSpreadDF, priceforNAVfilling =polygonApi.assemblePolygonData(etfData.getSymbols())
 
-    # for i in range(9, 16):
-    #     print("Hour at=" + str(i))
-    #     res = RunarbitrageObject.getMeHourdata(getmeHourDataFor=i, tradePricesDF=tradePricesDF,
-    #                                            quotesSpreadDF=quotesSpreadDF)
-    #     res['Arbitrage in $'] = abs(res['Arbitrage in $'])
-    #     res['Flag'] = 0
-    #     res.loc[(res['Arbitrage in $'] > res['ETF Trading Spread in $']) & res[
-    #         'ETF Trading Spread in $'] != 0, 'Flag'] = 111
-    #     print(res)
+    runArbitrageObject=RunArbitrage(etfname=etfname)
+    runArbitrageObject.giveArbitrageResults(tradeData=tradePricesDF,quotesData=quotesSpreadDF,priceforNAVfilling=priceforNAVfilling)
     
+    for i in range(9, 16):
+        print("Hour at=" + str(i))
+        try:
+            res = runArbitrageObject.getMeHourdata(getmeHourDataFor=i,etfData=etfData,
+                tradePricesDFMinutes=runArbitrageObject.tradePricesDFMinutes, 
+                quotesSpreadsMinutes=runArbitrageObject.quotesSpreadsMinutes)
+
+            res['Arbitrage in $'] = abs(res['Arbitrage in $'])
+            res['Flag'] = 0
+            res.loc[(res['Arbitrage in $'] > res['ETF Trading Spread in $']) & res['ETF Trading Spread in $'] != 0, 'Flag'] = 111
+            print(res)
+        except KeyError:
+            print("No Spread data was found for the ETF in this hour")
--- CalculateETFArbitrage/CreateURLSForThreading.py
+++ CalculateETFArbitrage/CreateURLSForThreading.py
@@ -10,6 +10,9 @@ from ThreadingRequests import main
 import logging
 import asyncio
 import datetime
+import pandas as pd
+import sys
+import time
 
 class CallPolygonApi(object):
 	
@@ -19,58 +22,80 @@ class CallPolygonApi(object):
 		self.starttime = starttime  # 9 AM
 		self.endtime = endtime  # 5 PM
 		self.endtimeLoop = endtimeLoop  # 4 PM
-		
+		self.extractDataTillTime = self.helperObj.stringTimeToDatetime(date=self.date, time=self.endtimeLoop)
+		self.endTs=self.helperObj.convertHumanTimeToUnixTimeStamp(date=self.date,time=self.endtime)
 		self.tickHistDataQuotes = None
 		self.tickHistDataTrade = None
 		self.priceforNAVfilling = None
 
-	'''
-	# 1) Paginated Request - If it doesn't need pagination, save it in a dictionary 
-	if pagination not required:
-		tradesData['AAPL']=data
-	else:
-		# Pagiation required
-		# Create a new set of urls and call getDataFromPolygon
-		# 
-	'''
-
-	def getDataFromPolygon(self,getUrls):
+	def getDataFromPolygon(self,getUrls=None,columnsNeeded=None,finalDf=None,PolygonMethod=None):
 		responses=main(getUrls)
-		c=0
-		for response in responses:
-			print(self.helperObj.getLastTimeStamp(response))
-			c+=1
-			print(c)
-			print(response['results'][0])
+		paginatedURLS=[]
 
-	
+		for response in responses:
+			
+			start_time=time.time()
+			symbol=response['ticker']
+			print("Time to get Symbol--- %s seconds ---" % (time.time() - start_time))
+
+			start_time=time.time()
+			tempdf=pd.DataFrame.from_dict(response['results'])[columnsNeeded]
+			print("Time to build tempdf--- %s seconds ---" % (time.time() - start_time))
+
+			tempdf['Symbol']=symbol
+
+			start_time=time.time()
+			finalDf=finalDf.append(tempdf)
+			print("Time to append to finaldf--- %s seconds ---" % (time.time() - start_time))
+
+			start_time=time.time()
+			lastUnixTimeStamp=self.helperObj.getLastTimeStamp(response)
+			print("Time to get lastUnixTimeStamp--- %s seconds ---" % (time.time() - start_time))
+
+			if self.helperObj.checkTimeStampForPagination(lastUnixTimeStamp,self.extractDataTillTime):
+				paginatedURLS.append(PolygonMethod(date=self.date, symbol=symbol,startTS=str(lastUnixTimeStamp),endTS=self.endTs,limitresult=str(50000)))
+				
+		if len(paginatedURLS)>0:
+			finalDf=self.getDataFromPolygon(getUrls=paginatedURLS,columnsNeeded=columnsNeeded,finalDf=finalDf,PolygonMethod=PolygonMethod)
+		return finalDf
+			
+	def getOpenCloseData(self,openCloseURLs=None):
+		responses=main(openCloseURLs)
+		priceforNAVfilling={}
+		for response in responses:
+			priceforNAVfilling[response['symbol']] = response['open']
+		return priceforNAVfilling
+
 	# Pass here etfdata object
 	def assemblePolygonData(self,symbols):
 	# Objects for polygon requests
 		Polygonobj = PolgonData()
 		# Get all tickers for the ETF
-		endTs=self.helperObj.convertHumanTimeToUnixTimeStamp(date=self.date,time=self.endtime)
+		trade_routines = [Polygonobj.PolygonHistoricTrades(date=self.date, symbol=symbol,startTS=None,endTS=self.endTs,limitresult=str(50000)) for symbol in symbols]
+		quotes_routines = [Polygonobj.PolygonHistoricQuotes(date=self.date, symbol=symbol,startTS=None,endTS=self.endTs,limitresult=str(50000)) for symbol in ['XLK']]
+		openCloseURLs =[Polygonobj.PolygonDailyOpenClose(date=self.date, symbol=symbol) for symbol in symbols]
 
-		trade_routines = [Polygonobj.PolygonHistoricTrades(date=self.date, symbol=symbol,startTS=None,endTS=endTs,limitresult=str(5000)) for symbol in symbols]
-		quotes_routines = [Polygonobj.PolygonHistoricQuotes(date=self.date, symbol=symbol,startTS=None,endTS=endTs,limitresult=str(5000)) for symbol in symbols]
-		openclose_routines =[Polygonobj.PolygonDailyOpenClose(date=self.date, symbol=symbol) for symbol in symbols]
+		tradesDataDf=pd.DataFrame(columns = ['p','s','t','x','Symbol'])
+		tradesDataDf=self.getDataFromPolygon(getUrls=trade_routines,columnsNeeded=['p','s','t','x'],finalDf=tradesDataDf,PolygonMethod=Polygonobj.PolygonHistoricTrades)
+		print(tradesDataDf)
+		print(tradesDataDf['Symbol'].unique())
+		
+		quotesDataDf=pd.DataFrame(columns = ['P','S','p','s','t','X','x','Symbol'])
+		quotesDataDf=self.getDataFromPolygon(getUrls=quotes_routines,columnsNeeded=['P','S','p','s','t','X','x'],finalDf=quotesDataDf,PolygonMethod=Polygonobj.PolygonHistoricQuotes)
+		print(quotesDataDf)
+		print(quotesDataDf['Symbol'].unique())
+		
+		priceforNAVfilling=self.getOpenCloseData(openCloseURLs=openCloseURLs)
+		print(priceforNAVfilling)
 
-		print("Trades Data")
-		self.getDataFromPolygon(trade_routines)
-		self.getDataFromPolygon(quotes_routines)
-		#self.getDataFromPolygon(openclose_routines)
-		'''
-		print("Quotes Data")
-		self.getDataFromPolygon(quotes_routines)
-		print("Open Close Data")
-		self.getDataFromPolygon(openclose_routines)
-		'''
+		return tradesDataDf, quotesDataDf, priceforNAVfilling
 
 
+''''
 if __name__ == "__main__":
 	# Create an object of date when we need and time between which we need data
-	previousdate = '2020-02-25'
-	date = '2020-02-26'
+	previousdate = '2020-03-08'
+	date = '2020-03-09'
 	starttime = '9:30:00'
 	endtime = '17:00:00'
 	endtimeLoop = '16:00:00'
@@ -80,7 +105,7 @@ if __name__ == "__main__":
 
 	polygonApi=CallPolygonApi(date=date)
 	polygonApi.assemblePolygonData(etfData.getSymbols())
-
+'''
 
 
 

e06924c60f8dad5525295db0a0a2db95ffa3a50a
piyush888 <piyush888@gmail.com>
2020-03-12 18:22:24 +0530

    Rectified CronTab code

--- CrontabForHoldingsDB.py
+++ CrontabForHoldingsDB.py
@@ -14,8 +14,8 @@ job = my_cron.new(
     command='cd /home/ubuntu/ETFAnalysis/ && /home/ubuntu/etfenv/bin/python3 /home/' + username + '/ETFAnalysis/ProcessCaller.py',
     comment='HoldingsCron')
 
-# specify time parameter = job to run 'on' every '9'th 'hour'
-job.hour.on(4)
+# specify time parameter = job to run 'on' every '9'th 'hour' UTC
+job.hour.on(9)
 
 # write to system crontab
 my_cron.write()

c03f2cad7cd54043531de9698386d4b7397fc7a4
piyush888 <piyush888@gmail.com>
2020-03-25 20:44:11 +0530

    Reset_index-->Time as a column in arb data-->Done

--- CalculateETFArbitrage/Caller.py
+++ CalculateETFArbitrage/Caller.py
@@ -8,15 +8,14 @@ from CalculateETFArbitrage.Control import ArbitrageCalculation
 from MongoDB.SaveArbitrageCalcs import SaveCalculatedArbitrage
 
 try:
-    etflist = ['XLY', 'XLC', 'VCR', 'ITB', 'IYC', 'FDIS', 'XRT', 'FXD', 'CHIQ', 'ESPO', 'RTH', 'HERO', 'INCO',
-               'VMOT',
-               'RCD', 'PEJ', 'JHMC', 'PBS', 'NAIL', 'ONLN', 'BFIT', 'BJK', 'UCC', 'PEZ', 'CARZ', 'PSCD', 'IEDI']
+    etflist = ['XLK','XLY', 'XLC', 'VCR', 'ITB', 'IYC', 'FDIS', 'XRT', 'FXD']
+    # etflist = ['CHIQ']
     date = '2020-03-16'
     for etfname in etflist:
         data = ArbitrageCalculation().calculateArbitrage(etfname, date)
+        data.reset_index(inplace=True)
         SaveCalculatedArbitrage().insertIntoCollection(etfname, datetime.now().date().strftime('%Y-%m-%d'),
                                                        data.to_dict(orient='records'))
 except Exception as e:
     print("exception in {} etf".format(etfname))
     print(e)
-

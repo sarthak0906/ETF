c921164413137627daaced79c847675182fd3c71
piyush888 <piyush888@gmail.com>
2020-03-25 19:09:15 +0530

    Converted ArbitrageCalculation to func, Saving output to DB

--- CalculateETFArbitrage/Caller.py
+++ CalculateETFArbitrage/Caller.py
@@ -7,19 +7,16 @@ from datetime import datetime
 from CalculateETFArbitrage.Control import ArbitrageCalculation
 from MongoDB.SaveArbitrageCalcs import SaveCalculatedArbitrage
 
-dfs = {}
 try:
     etflist = ['XLY', 'XLC', 'VCR', 'ITB', 'IYC', 'FDIS', 'XRT', 'FXD', 'CHIQ', 'ESPO', 'RTH', 'HERO', 'INCO',
                'VMOT',
                'RCD', 'PEJ', 'JHMC', 'PBS', 'NAIL', 'ONLN', 'BFIT', 'BJK', 'UCC', 'PEZ', 'CARZ', 'PSCD', 'IEDI']
     date = '2020-03-16'
     for etfname in etflist:
-        dfs[etfname] = ArbitrageCalculation().calculateArbitrage(etfname, date)
+        data = ArbitrageCalculation().calculateArbitrage(etfname, date)
         SaveCalculatedArbitrage().insertIntoCollection(etfname, datetime.now().date().strftime('%Y-%m-%d'),
-                                                       dfs[etfname].to_dict(orient='records'))
-    print(len(dfs.values()))
+                                                       data.to_dict(orient='records'))
 except Exception as e:
     print("exception in {} etf".format(etfname))
     print(e)
-finally:
-    print(len(dfs.values()))
+

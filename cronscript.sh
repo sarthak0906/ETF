96db60ac29d77c0dccb77359b93ac713b1b188c0
piyush888 <piyush888@gmail.com>
2020-04-04 21:51:42 +0530

    New cronscript.sh with ArbCalc for production

--- cronscript.sh
+++ cronscript.sh
@@ -2,3 +2,5 @@
 source /home/ubuntu/etfenv/bin/activate
 cd /home/ubuntu/ETFAnalysis/ || exit
 python ProcessCaller.py
+wait $(pidof python)
+python /home/ubuntu/ETFAnalysis/CalculateETFArbitrage/Caller.py

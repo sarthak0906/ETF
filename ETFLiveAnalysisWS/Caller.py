e955bc8d66897fcb4ba4bd5b8bfedfe97ff38d79
piyush888 <piyush888@gmail.com>
2020-05-15 22:11:05 +0530

    PerMinArbSpread: v1.0: Rectified Paths

--- ETFLiveAnalysisWS/Caller.py
+++ ETFLiveAnalysisWS/Caller.py
@@ -1,3 +1,14 @@
+import sys, traceback
+# For Piyush System
+sys.path.extend(['/home/piyush/Desktop/etf1903', '/home/piyush/Desktop/etf1903/ETFsList_Scripts',
+                 '/home/piyush/Desktop/etf1903/HoldingsDataScripts',
+                 '/home/piyush/Desktop/etf1903/CommonServices',
+                 '/home/piyush/Desktop/etf1903/CalculateETFArbitrage','/home/piyush/Desktop/etf1903/ETFLiveAnalysis'])
+# For Production env
+sys.path.extend(['/home/ubuntu/ETFAnalysis', '/home/ubuntu/ETFAnalysis/ETFsList_Scripts',
+                 '/home/ubuntu/ETFAnalysis/HoldingsDataScripts', '/home/ubuntu/ETFAnalysis/CommonServices',
+                 '/home/ubuntu/ETFAnalysis/CalculateETFArbitrage'])
+sys.path.append("..")  # Remove in production - KTZ
 import datetime
 import time
 import schedule

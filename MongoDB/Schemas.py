f3c5af1663a3d13d8a828ba0b2a29de81d26abb4
piyush888 <piyush888@gmail.com>
2020-05-16 15:14:07 +0530

    Cleaned commented code, Added Comments for new code

--- MongoDB/Schemas.py
+++ MongoDB/Schemas.py
@@ -60,19 +60,11 @@ arbitrage_per_min = db.ArbitragePerMin
 arbitrage_per_min.create_index([('DateTimeOfArbitrage', DESCENDING)])
 arbitrage_per_min.create_index([('ETFName', ASCENDING)])
 
+# Trade Aggregate Minute for all Tickers.
+# Cursor for pulling data (PyMongo Cursor)
 trade_per_min_WS = db.TradePerMinWS
-# trade_per_min_WS.create_index([("end_ts", DESCENDING), ("symbol", ASCENDING)], unique=True)
-# trade_per_min_WS.create_index([('end_ts', DESCENDING)])
-# trade_per_min_WS.create_index([('symbol', ASCENDING)])
-
+# Cursor for insert action into TradePerMinWS (AsyncIOMotorCursor)
 trade_per_min_WS_motor = motor_db.TradePerMinWS
-
-# async def create_index_for_motorcolls(coll, indexterm, order=ASCENDING):
-#     await coll.create_index([indexterm,order])
-# loop1 = asyncio.get_event_loop()
-# task1 = asyncio.ensure_future(create_index_for_motorcolls(trade_per_min_WS_motor,'sym',ASCENDING))
-# task2 = asyncio.ensure_future(create_index_for_motorcolls(trade_per_min_WS_motor,'e',DESCENDING))
-# loop1.run_until_complete(task1,task2)
 
 quotesWS_collection = db.QuotesLiveData
 

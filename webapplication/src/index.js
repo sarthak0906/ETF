f689fa4942033236e73fa5515b1ecc3d93e3fa16
sarthak0906 <sarthak0906@gmail.com>
2020-05-18 01:20:27 +0530

    adding the ETF selected in the selected SPDR

--- webapplication/src/index.js
+++ webapplication/src/index.js
@@ -1,8 +1,7 @@
 import React, {useState } from 'react';
 import { render } from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import Former from './Component/Form.js';
-import Analysis from './Component/ETF-Analysis';
 import Comparison from './Component/ETF-Comparison';
 import Description from './Component/ETF-Description';
 import Historical from './Component/Historical-Arbitrage';
@@ -39,11 +38,8 @@ const App = (props) => {
   const SubmitFn = async (stock, date) => {
     await setETF(stock);
     await setDate(date);
-    // console.log(date);
-    // console.log(ETF);
     let a = stock + '-'  +date;
     await setFile(a);
-    // console.log(file);
   }
 
   return (
@@ -51,12 +47,11 @@ const App = (props) => {
       <div className="Container">
         <div>
           <div className="Form">
-            <Former submitFn={SubmitFn} />
+            <Former submitFn={SubmitFn} ETF={ETF} />
           </div>
         </div>
       </div>
       {/* <Route exact path="/" render={} /> */}
-      {/* <Route path="/ETF-Analysis" render={Analysis} /> */}
       <Route path="/ETF-Comparison" render={Comparison} />
       <Route path="/ETF-Description" render={() => <Description file={file} startDate ={startDate} ETF={ETF} submitFn={SubmitFn} />} />
       <Route path="/Historical" render={Historical} />

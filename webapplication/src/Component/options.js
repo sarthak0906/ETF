6e556d389c616bc809d240701db5156d0f0377b0
sarthak khandelwal <sarthak0906@gmail.com>
2020-05-28 00:19:05 +0530

    ETF select option changed to newer design

---
+++ webapplication/src/Component/options.js
@@ -0,0 +1,1130 @@
+export const options = [
+    {
+        "element": "XLK",
+        "index": 0
+    },
+    {
+        "element": "BMLP",
+        "index": 1
+    },
+    {
+        "element": "IHI",
+        "index": 2
+    },
+    {
+        "element": "SZK",
+        "index": 3
+    },
+    {
+        "element": "JHMC",
+        "index": 4
+    },
+    {
+        "element": "FHLC",
+        "index": 5
+    },
+    {
+        "element": "PUI",
+        "index": 6
+    },
+    {
+        "element": "KBWB",
+        "index": 7
+    },
+    {
+        "element": "FTXG",
+        "index": 8
+    },
+    {
+        "element": "XLV",
+        "index": 9
+    },
+    {
+        "element": "XBI",
+        "index": 10
+    },
+    {
+        "element": "RORE",
+        "index": 11
+    },
+    {
+        "element": "PSCM",
+        "index": 12
+    },
+    {
+        "element": "PASS",
+        "index": 13
+    },
+    {
+        "element": "IEO",
+        "index": 14
+    },
+    {
+        "element": "IYZ",
+        "index": 15
+    },
+    {
+        "element": "XLY",
+        "index": 16
+    },
+    {
+        "element": "PSCF",
+        "index": 17
+    },
+    {
+        "element": "MRRL",
+        "index": 18
+    },
+    {
+        "element": "PXE",
+        "index": 19
+    },
+    {
+        "element": "BNKU",
+        "index": 20
+    },
+    {
+        "element": "XLC",
+        "index": 21
+    },
+    {
+        "element": "XHB",
+        "index": 22
+    },
+    {
+        "element": "PPH",
+        "index": 23
+    },
+    {
+        "element": "XRT",
+        "index": 24
+    },
+    {
+        "element": "NUGT",
+        "index": 25
+    },
+    {
+        "element": "FDIS",
+        "index": 26
+    },
+    {
+        "element": "XWEB",
+        "index": 27
+    },
+    {
+        "element": "FDN",
+        "index": 28
+    },
+    {
+        "element": "RTM",
+        "index": 29
+    },
+    {
+        "element": "SEF",
+        "index": 30
+    },
+    {
+        "element": "USD",
+        "index": 31
+    },
+    {
+        "element": "QABA",
+        "index": 32
+    },
+    {
+        "element": "DRN",
+        "index": 33
+    },
+    {
+        "element": "KBE",
+        "index": 34
+    },
+    {
+        "element": "TAWK",
+        "index": 35
+    },
+    {
+        "element": "IBB",
+        "index": 36
+    },
+    {
+        "element": "UGE",
+        "index": 37
+    },
+    {
+        "element": "PSCE",
+        "index": 38
+    },
+    {
+        "element": "SCC",
+        "index": 39
+    },
+    {
+        "element": "WANT",
+        "index": 40
+    },
+    {
+        "element": "RXL",
+        "index": 41
+    },
+    {
+        "element": "EVX",
+        "index": 42
+    },
+    {
+        "element": "KRE",
+        "index": 43
+    },
+    {
+        "element": "DRV",
+        "index": 44
+    },
+    {
+        "element": "BNKD",
+        "index": 45
+    },
+    {
+        "element": "BTEC",
+        "index": 46
+    },
+    {
+        "element": "ROKT",
+        "index": 47
+    },
+    {
+        "element": "BBC",
+        "index": 48
+    },
+    {
+        "element": "SOXS",
+        "index": 49
+    },
+    {
+        "element": "GASX",
+        "index": 50
+    },
+    {
+        "element": "FXZ",
+        "index": 51
+    },
+    {
+        "element": "CNRG",
+        "index": 52
+    },
+    {
+        "element": "PTH",
+        "index": 53
+    },
+    {
+        "element": "XHE",
+        "index": 54
+    },
+    {
+        "element": "XLP",
+        "index": 55
+    },
+    {
+        "element": "HDGE",
+        "index": 56
+    },
+    {
+        "element": "FXD",
+        "index": 57
+    },
+    {
+        "element": "FTEC",
+        "index": 58
+    },
+    {
+        "element": "REZ",
+        "index": 59
+    },
+    {
+        "element": "XITK",
+        "index": 60
+    },
+    {
+        "element": "TECS",
+        "index": 61
+    },
+    {
+        "element": "IHF",
+        "index": 62
+    },
+    {
+        "element": "DUSL",
+        "index": 63
+    },
+    {
+        "element": "XLF",
+        "index": 64
+    },
+    {
+        "element": "ZIG",
+        "index": 65
+    },
+    {
+        "element": "IYG",
+        "index": 66
+    },
+    {
+        "element": "PBE",
+        "index": 67
+    },
+    {
+        "element": "DDG",
+        "index": 68
+    },
+    {
+        "element": "ZBIO",
+        "index": 69
+    },
+    {
+        "element": "SCHH",
+        "index": 70
+    },
+    {
+        "element": "FINU",
+        "index": 71
+    },
+    {
+        "element": "SOXX",
+        "index": 72
+    },
+    {
+        "element": "XLB",
+        "index": 73
+    },
+    {
+        "element": "FRI",
+        "index": 74
+    },
+    {
+        "element": "IECS",
+        "index": 75
+    },
+    {
+        "element": "XPH",
+        "index": 76
+    },
+    {
+        "element": "IYC",
+        "index": 77
+    },
+    {
+        "element": "SIJ",
+        "index": 78
+    },
+    {
+        "element": "SKYY",
+        "index": 79
+    },
+    {
+        "element": "IYE",
+        "index": 80
+    },
+    {
+        "element": "PKB",
+        "index": 81
+    },
+    {
+        "element": "RETL",
+        "index": 82
+    },
+    {
+        "element": "RYU",
+        "index": 83
+    },
+    {
+        "element": "FTXL",
+        "index": 84
+    },
+    {
+        "element": "PPA",
+        "index": 85
+    },
+    {
+        "element": "PYZ",
+        "index": 86
+    },
+    {
+        "element": "FXU",
+        "index": 87
+    },
+    {
+        "element": "AMZA",
+        "index": 88
+    },
+    {
+        "element": "RWR",
+        "index": 89
+    },
+    {
+        "element": "FMAT",
+        "index": 90
+    },
+    {
+        "element": "XLI",
+        "index": 91
+    },
+    {
+        "element": "REK",
+        "index": 92
+    },
+    {
+        "element": "ITB",
+        "index": 93
+    },
+    {
+        "element": "BBH",
+        "index": 94
+    },
+    {
+        "element": "RYE",
+        "index": 95
+    },
+    {
+        "element": "BBP",
+        "index": 96
+    },
+    {
+        "element": "IEDI",
+        "index": 97
+    },
+    {
+        "element": "IAI",
+        "index": 98
+    },
+    {
+        "element": "PSCT",
+        "index": 99
+    },
+    {
+        "element": "TDV",
+        "index": 100
+    },
+    {
+        "element": "XLE",
+        "index": 101
+    },
+    {
+        "element": "JHMA",
+        "index": 102
+    },
+    {
+        "element": "LABU",
+        "index": 103
+    },
+    {
+        "element": "PPTY",
+        "index": 104
+    },
+    {
+        "element": "JHME",
+        "index": 105
+    },
+    {
+        "element": "XSD",
+        "index": 106
+    },
+    {
+        "element": "VPC",
+        "index": 107
+    },
+    {
+        "element": "REML",
+        "index": 108
+    },
+    {
+        "element": "KBWP",
+        "index": 109
+    },
+    {
+        "element": "DIG",
+        "index": 110
+    },
+    {
+        "element": "UYM",
+        "index": 111
+    },
+    {
+        "element": "EWRE",
+        "index": 112
+    },
+    {
+        "element": "DPST",
+        "index": 113
+    },
+    {
+        "element": "ROOF",
+        "index": 114
+    },
+    {
+        "element": "MORL",
+        "index": 115
+    },
+    {
+        "element": "FTXR",
+        "index": 116
+    },
+    {
+        "element": "FUTY",
+        "index": 117
+    },
+    {
+        "element": "XOP",
+        "index": 118
+    },
+    {
+        "element": "PSL",
+        "index": 119
+    },
+    {
+        "element": "PXJ",
+        "index": 120
+    },
+    {
+        "element": "PSI",
+        "index": 121
+    },
+    {
+        "element": "FTXO",
+        "index": 122
+    },
+    {
+        "element": "IGN",
+        "index": 123
+    },
+    {
+        "element": "RTH",
+        "index": 124
+    },
+    {
+        "element": "IHE",
+        "index": 125
+    },
+    {
+        "element": "FXH",
+        "index": 126
+    },
+    {
+        "element": "BBRE",
+        "index": 127
+    },
+    {
+        "element": "PSCI",
+        "index": 128
+    },
+    {
+        "element": "ROM",
+        "index": 129
+    },
+    {
+        "element": "VCR",
+        "index": 130
+    },
+    {
+        "element": "NAIL",
+        "index": 131
+    },
+    {
+        "element": "FIVG",
+        "index": 132
+    },
+    {
+        "element": "MLPQ",
+        "index": 133
+    },
+    {
+        "element": "ONLN",
+        "index": 134
+    },
+    {
+        "element": "QQQ",
+        "index": 135
+    },
+    {
+        "element": "NRGO",
+        "index": 136
+    },
+    {
+        "element": "AMLP",
+        "index": 137
+    },
+    {
+        "element": "XLU",
+        "index": 138
+    },
+    {
+        "element": "FBT",
+        "index": 139
+    },
+    {
+        "element": "JHMS",
+        "index": 140
+    },
+    {
+        "element": "SKF",
+        "index": 141
+    },
+    {
+        "element": "PJP",
+        "index": 142
+    },
+    {
+        "element": "VNQ",
+        "index": 143
+    },
+    {
+        "element": "VGT",
+        "index": 144
+    },
+    {
+        "element": "INDS",
+        "index": 145
+    },
+    {
+        "element": "CWEB",
+        "index": 146
+    },
+    {
+        "element": "SMN",
+        "index": 147
+    },
+    {
+        "element": "WCLD",
+        "index": 148
+    },
+    {
+        "element": "XTN",
+        "index": 149
+    },
+    {
+        "element": "ERY",
+        "index": 150
+    },
+    {
+        "element": "UTES",
+        "index": 151
+    },
+    {
+        "element": "HOMZ",
+        "index": 152
+    },
+    {
+        "element": "RHS",
+        "index": 153
+    },
+    {
+        "element": "FIDU",
+        "index": 154
+    },
+    {
+        "element": "RDOG",
+        "index": 155
+    },
+    {
+        "element": "UXI",
+        "index": 156
+    },
+    {
+        "element": "SRS",
+        "index": 157
+    },
+    {
+        "element": "IYF",
+        "index": 158
+    },
+    {
+        "element": "TPOR",
+        "index": 159
+    },
+    {
+        "element": "PTF",
+        "index": 160
+    },
+    {
+        "element": "VDE",
+        "index": 161
+    },
+    {
+        "element": "IGE",
+        "index": 162
+    },
+    {
+        "element": "JHMH",
+        "index": 163
+    },
+    {
+        "element": "DUG",
+        "index": 164
+    },
+    {
+        "element": "PBW",
+        "index": 165
+    },
+    {
+        "element": "CLIX",
+        "index": 166
+    },
+    {
+        "element": "JNUG",
+        "index": 167
+    },
+    {
+        "element": "NURE",
+        "index": 168
+    },
+    {
+        "element": "MLPA",
+        "index": 169
+    },
+    {
+        "element": "XNTK",
+        "index": 170
+    },
+    {
+        "element": "IYM",
+        "index": 171
+    },
+    {
+        "element": "FIW",
+        "index": 172
+    },
+    {
+        "element": "PXQ",
+        "index": 173
+    },
+    {
+        "element": "QTEC",
+        "index": 174
+    },
+    {
+        "element": "OIH",
+        "index": 175
+    },
+    {
+        "element": "KIE",
+        "index": 176
+    },
+    {
+        "element": "FINZ",
+        "index": 177
+    },
+    {
+        "element": "IYT",
+        "index": 178
+    },
+    {
+        "element": "XSW",
+        "index": 179
+    },
+    {
+        "element": "LABD",
+        "index": 180
+    },
+    {
+        "element": "XHS",
+        "index": 181
+    },
+    {
+        "element": "JHMU",
+        "index": 182
+    },
+    {
+        "element": "IAT",
+        "index": 183
+    },
+    {
+        "element": "PEZ",
+        "index": 184
+    },
+    {
+        "element": "IFRA",
+        "index": 185
+    },
+    {
+        "element": "UPW",
+        "index": 186
+    },
+    {
+        "element": "IEFN",
+        "index": 187
+    },
+    {
+        "element": "FXR",
+        "index": 188
+    },
+    {
+        "element": "RGI",
+        "index": 189
+    },
+    {
+        "element": "ZMLP",
+        "index": 190
+    },
+    {
+        "element": "VHT",
+        "index": 191
+    },
+    {
+        "element": "XAR",
+        "index": 192
+    },
+    {
+        "element": "IGM",
+        "index": 193
+    },
+    {
+        "element": "RYT",
+        "index": 194
+    },
+    {
+        "element": "IYW",
+        "index": 195
+    },
+    {
+        "element": "IYK",
+        "index": 196
+    },
+    {
+        "element": "IEHS",
+        "index": 197
+    },
+    {
+        "element": "UCC",
+        "index": 198
+    },
+    {
+        "element": "VAW",
+        "index": 199
+    },
+    {
+        "element": "XME",
+        "index": 200
+    },
+    {
+        "element": "IEZ",
+        "index": 201
+    },
+    {
+        "element": "PHO",
+        "index": 202
+    },
+    {
+        "element": "ITA",
+        "index": 203
+    },
+    {
+        "element": "GASL",
+        "index": 204
+    },
+    {
+        "element": "SMH",
+        "index": 205
+    },
+    {
+        "element": "VPU",
+        "index": 206
+    },
+    {
+        "element": "PSCD",
+        "index": 207
+    },
+    {
+        "element": "PSCH",
+        "index": 208
+    },
+    {
+        "element": "PSJ",
+        "index": 209
+    },
+    {
+        "element": "SDP",
+        "index": 210
+    },
+    {
+        "element": "NEED",
+        "index": 211
+    },
+    {
+        "element": "PNQI",
+        "index": 212
+    },
+    {
+        "element": "SOXL",
+        "index": 213
+    },
+    {
+        "element": "SSG",
+        "index": 214
+    },
+    {
+        "element": "KBWR",
+        "index": 215
+    },
+    {
+        "element": "SRVR",
+        "index": 216
+    },
+    {
+        "element": "XLRE",
+        "index": 217
+    },
+    {
+        "element": "FCG",
+        "index": 218
+    },
+    {
+        "element": "VIS",
+        "index": 219
+    },
+    {
+        "element": "JHMT",
+        "index": 220
+    },
+    {
+        "element": "IAK",
+        "index": 221
+    },
+    {
+        "element": "KBWY",
+        "index": 222
+    },
+    {
+        "element": "MLPX",
+        "index": 223
+    },
+    {
+        "element": "ICF",
+        "index": 224
+    },
+    {
+        "element": "PXI",
+        "index": 225
+    },
+    {
+        "element": "PAVE",
+        "index": 226
+    },
+    {
+        "element": "REW",
+        "index": 227
+    },
+    {
+        "element": "HAIL",
+        "index": 228
+    },
+    {
+        "element": "DRIP",
+        "index": 229
+    },
+    {
+        "element": "SLX",
+        "index": 230
+    },
+    {
+        "element": "PILL",
+        "index": 231
+    },
+    {
+        "element": "PBS",
+        "index": 232
+    },
+    {
+        "element": "AIRR",
+        "index": 233
+    },
+    {
+        "element": "IDU",
+        "index": 234
+    },
+    {
+        "element": "FITE",
+        "index": 235
+    },
+    {
+        "element": "IYH",
+        "index": 236
+    },
+    {
+        "element": "REM",
+        "index": 237
+    },
+    {
+        "element": "ERX",
+        "index": 238
+    },
+    {
+        "element": "MLPB",
+        "index": 239
+    },
+    {
+        "element": "PEJ",
+        "index": 240
+    },
+    {
+        "element": "NRGU",
+        "index": 241
+    },
+    {
+        "element": "XTL",
+        "index": 242
+    },
+    {
+        "element": "BIZD",
+        "index": 243
+    },
+    {
+        "element": "ARKG",
+        "index": 244
+    },
+    {
+        "element": "NETL",
+        "index": 245
+    },
+    {
+        "element": "RCD",
+        "index": 246
+    },
+    {
+        "element": "SIMS",
+        "index": 247
+    },
+    {
+        "element": "IETC",
+        "index": 248
+    },
+    {
+        "element": "RYH",
+        "index": 249
+    },
+    {
+        "element": "RYF",
+        "index": 250
+    },
+    {
+        "element": "NRGD",
+        "index": 251
+    },
+    {
+        "element": "VDC",
+        "index": 252
+    },
+    {
+        "element": "XES",
+        "index": 253
+    },
+    {
+        "element": "MORT",
+        "index": 254
+    },
+    {
+        "element": "FREL",
+        "index": 255
+    },
+    {
+        "element": "FENY",
+        "index": 256
+    },
+    {
+        "element": "TDIV",
+        "index": 257
+    },
+    {
+        "element": "FXG",
+        "index": 258
+    },
+    {
+        "element": "PFI",
+        "index": 259
+    },
+    {
+        "element": "BUYN",
+        "index": 260
+    },
+    {
+        "element": "FXL",
+        "index": 261
+    },
+    {
+        "element": "PSR",
+        "index": 262
+    },
+    {
+        "element": "PSCU",
+        "index": 263
+    },
+    {
+        "element": "SBIO",
+        "index": 264
+    },
+    {
+        "element": "PRN",
+        "index": 265
+    },
+    {
+        "element": "FSTA",
+        "index": 266
+    },
+    {
+        "element": "DFEN",
+        "index": 267
+    },
+    {
+        "element": "IYR",
+        "index": 268
+    },
+    {
+        "element": "VOX",
+        "index": 269
+    },
+    {
+        "element": "FXN",
+        "index": 270
+    },
+    {
+        "element": "GUSH",
+        "index": 271
+    },
+    {
+        "element": "CNCR",
+        "index": 272
+    },
+    {
+        "element": "MLPI",
+        "index": 273
+    },
+    {
+        "element": "URE",
+        "index": 274
+    },
+    {
+        "element": "CURE",
+        "index": 275
+    },
+    {
+        "element": "LACK",
+        "index": 276
+    },
+    {
+        "element": "UTSL",
+        "index": 277
+    },
+    {
+        "element": "VMOT",
+        "index": 278
+    },
+    {
+        "element": "IGV",
+        "index": 279
+    },
+    {
+        "element": "WDRW",
+        "index": 280
+    },
+    {
+        "element": "TECL",
+        "index": 281
+    }
+];
